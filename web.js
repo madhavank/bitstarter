ab302935951dc52ca9314fc3243cf0aee0e8da26
Madhavan Krishnan <madhavan.tsk@gmail.com>
2013-07-09 10:30:09 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
-	var k = fs.readFileSync('/home/ubuntu/bitstarter/index.html','utf8');
+	var k = fs.readFileSync('./index.html','utf8');
 	console.log(" The string value is " + k);
 	response.send(k.toString('utf8',0,buffer.length));
 });
