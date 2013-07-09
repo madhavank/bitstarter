907099840994f6b746cdd1ad63522181703b0a05
Madhavan Krishnan <madhavan.tsk@gmail.com>
2013-07-09 10:34:50 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	var k = fs.readFileSync('./index.html','utf8');
 	console.log(" The string value is " + k);
-	response.send(k.toString('utf8',0,buffer.length));
+	response.send(k.toString('utf8',0,k.length));
 });
 
 var port = process.env.PORT || 5000;
