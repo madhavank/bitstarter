81891ac27fcb54c6658185c78dd617c0541e04d3
Madhavan Krishnan <madhavan.tsk@gmail.com>
2013-07-09 09:42:25 +0000

    read from index.html

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	var k = fs.readFileSync('/home/ubuntu/bitstarter/index.html','utf8');
 	console.log(" The string value is " + k);
-	response.send(str);
+	response.send(k);
 });
 
 var port = process.env.PORT || 5000;
