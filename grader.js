b53d7e85e75e56f38649ea35c7cee8607512a202
Madhavan Krishnan <madhavan.tsk@gmail.com>
2013-07-11 09:06:10 +0000

    updated grader.js for hw3 submission

--- grader.js
+++ grader.js
@@ -24,8 +24,13 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+// added for hw3
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "url.file"
+var OUT_FILE_NAME = "./outputfile.html"
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -45,6 +50,7 @@ var loadChecks = function(checksfile) {
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
+    console.log("passed HTML file = " + htmlfile);
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
@@ -61,14 +67,52 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var urlCheck = function() {
+	console.log("Control comes here");
+}
+
 if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url', 'Url of html file')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+/*
+
+    console.log("After parsing, process.argv is = " + process.argv[5]);
+    console.log("After parsing, program is = " + program);
+    console.log("After parsing, program.url is = " + program.url);
+    console.log("After parsing, program.file is = " + program.file);
+    console.log("After parsing, program.checks = " + program.checks);a
+*/
+//    console.log("After parsing, program.url is = " + program.url);
+   if(program.url)
+   {
+
+	//rest.get('https://github.com/madhavank/bitstarter/blob/master/index.html').on('complete',function(result) {
+	rest.get(process.argv[5]).on('complete',function(result) {
+		fs.writeFileSync(OUT_FILE_NAME, result);
+		program.file = OUT_FILE_NAME ;
+   	        console.log("Program.file assigned is " + program.file);	
+   	        console.log("Program.checks assigned is " + program.checks);	
+    		var checkJson = checkHtmlFile(program.file, program.checks);
+    		var outJson = JSON.stringify(checkJson, null, 4);
+//		console.log("result from rest.get function call is " + result) ;	
+		console.log("Wrote into output3.html file") ;
+    		console.log(outJson);
+	});
+
+   }
+   else
+   {
+     	console.log("Program.file just before passing to checkHtml is " + program.file)
+    	var checkJson = checkHtmlFile(program.file, program.checks);
+   	 var outJson = JSON.stringify(checkJson, null, 4);
+    	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
+
+//rest.get(apiurl).on('complete', response2console);
